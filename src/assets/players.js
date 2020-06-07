05f4bd9e91d6eb32bfe5fb49f722c5d340da25f8
Ryan How <ryanhow1999@yahoo.ca>
2020-06-07 00:12:06 -0400

    players data added

---
+++ src/assets/players.js
@@ -0,0 +1,114 @@
+export const players = [
+    {
+        firstName: "OG",
+        lastName: "Anunoby",
+        position: "Small Forward",
+        number: 3,
+        image: "og.png"
+    },
+    {
+        firstName: "Chris",
+        lastName: "Boucher",
+        position: "Power Forward",
+        number: 25,
+        image: "chris.png"
+    },
+    {
+        firstName: "Oshae",
+        lastName: "Brissett",
+        position: "Small Forward",
+        number: 12,
+        image: "oshae.png"
+    },
+    {
+        firstName: "Terence",
+        lastName: "Davis",
+        position: "Shooting Guard",
+        number: 0,
+        image: "terence.png"
+    },
+    {
+        firstName: "Marc",
+        lastName: "Gasol",
+        position: "Center",
+        number: 33,
+        image: "marc.png"
+    },
+    {
+        firstName: "Dewan",
+        lastName: "Hernandex",
+        position: "Center",
+        number: 20,
+        image: "dewan.png"
+    },
+    {
+        firstName: "Rondae",
+        lastName: "Hollis-Jefferson",
+        position: "Small Forward",
+        number: 4,
+        image: "rondae.png"
+    },
+    {
+        firstName: "Serge",
+        lastName: "Ibaka",
+        position: "Center",
+        number: 9,
+        image: "serge.png"
+    },
+    {
+        firstName: "Stanley",
+        lastName: "Johnson",
+        position: "Small Forward",
+        number: 5,
+        image: "stanley.png"
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Lowry",
+        position: "Point Guard",
+        number: 7,
+        image: "kyle.png"
+    },
+    {
+        firstName: "Patrick",
+        lastName: "McCaw",
+        position: "Small Foward",
+        number: 22,
+        image: "patrick.png"
+    },
+    {
+        firstName: "Malcolm",
+        lastName: "Miller",
+        position: "Shooting Guard",
+        number: 13,
+        image: "malcolm.png"
+    },
+    {
+        firstName: "Norman",
+        lastName: "Powell",
+        position: "Shooting Guard",
+        number: 24,
+        image: "norman.png"
+    },
+    {
+        firstName: "Pascal",
+        lastName: "Siakam",
+        position: "Power Forward",
+        number: 43,
+        image: "pascal.png"
+    },
+    {
+        firstName: "Matt",
+        lastName: "Thomas",
+        position: "Shooting Guard",
+        number: 21,
+        image: "matt.png"
+    },
+    {
+        firstName: "Fred",
+        lastName: "VanVleet",
+        position: "Shooting Guard",
+        number: 23,
+        image: "fred.png"
+    }
+]
---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5">
+                <img src=''></img>
+                <div>
+                    <h2>Norman Powell</h2>
+                    <p>Shooting Guard (SG)</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
